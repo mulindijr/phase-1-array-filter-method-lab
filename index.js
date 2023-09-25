7169900effc00c787ebf4f91126bae914552ad58
Brian Mulindi <mulindijrn@gmail.com>
2023-09-25 18:56:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// Function 1: findMatching
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Function 2: fuzzyMatch
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// Function 3: matchName
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
